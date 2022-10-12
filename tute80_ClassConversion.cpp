b1ded2ee0138e7dde42b84d4c78ab086e65349d1
Arunoday <arunodaysingh137@gmail.com>
2022-10-13 00:24:58 +0530

    new file:   tute80_ClassConversion.cpp

---
+++ tute80_ClassConversion.cpp
@@ -0,0 +1,8 @@
+// As we convert one primitive to another primitive type 
+// Same we can do with class and this is called type casting that's called Class conversion.
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ return 0;
+}
