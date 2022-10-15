64d6c2f80b8498041217db62ace546ce70391ef9
Arunoday <arunodaysingh137@gmail.com>
2022-10-15 17:12:59 +0530

    Changes to be committed: 	modified:   revision.cpp 	modified:   revision.exe

--- revision.cpp
+++ revision.cpp
@@ -1,20 +1,12 @@
 #include<iostream>
 using namespace std;
-class A{
-  public:
-  A() {cout<<"1st"<<endl;}
-  ~A() {cout<<"1st des"<<endl;}
-};
-class B:public A
-{
-  public:
-  B(){cout<<"Base class Constructor "<<endl;}
-  ~B(){cout<<"Base class Destructor"<<endl;}
-};
-
 int main()
 {
-B obj;
-return 0;
+register int i=1;
+int *ptr=&i;
+int a=i++;
+cout<<*ptr<<" "<<a;
 }
+//Flow of constructor and destructor
+
 
