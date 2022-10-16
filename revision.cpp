5fac1a3fd45546f043c531eff042014f5b73f378
Arunoday <arunodaysingh137@gmail.com>
2022-10-17 04:45:36 +0530

    modified:   revision.cpp 	modified:   revision.exe

--- revision.cpp
+++ revision.cpp
@@ -1,12 +1,52 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-register int i=1;
-int *ptr=&i;
-int a=i++;
-cout<<*ptr<<" "<<a;
-}
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// register int i=1;
+// int *ptr=&i;
+// int a=i++;
+// cout<<*ptr<<" "<<a;
+// }
 //Flow of constructor and destructor
 
+//Constructor flow & Destructor Flow
 
+#include<bits/stdc++.h>
+using namespace std;
+class A
+{
+    public:
+    A(){
+        cout<<"Constructor of the Base class "<<endl;
+    }
+    ~A()
+    {
+        cout<<"Destructor of the Base class "<<endl;
+    }
+};
+class B:public A
+{
+    public:
+    B()
+    {
+        cout<<"Constructor of the derived class "<<endl;
+    }
+    ~B()
+    {
+        cout<<"Destructor of the Derived class "<<endl;
+    }
+};
+int main()
+{
+    B obj;
+ return 0;
+}
+// constructor top to bottom 
+// destructor bottom to up
+// output 
+/*
+Constructor of the Base class
+Constructor of the derived class
+Destructor of the Derived class
+Destructor of the Base class
+*/
